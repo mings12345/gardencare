382ac2b212805a96d76fa98558a66b599c93b110
Jeffrey Gepiga <jeffreygepiga27@gmail.com>
2025-02-26 10:53:05 +0800

    Updated

--- routes/web.php
+++ routes/web.php
@@ -6,4 +6,14 @@ Route::get('/', function () {
     return ['Laravel' => app()->version()];
 });
 
+Route::get('/update-app', function () {
+    Artisan::call('update-app');
+    return "Successfully Updated";
+});
+
+Route::get('/reset-app', function () {
+    Artisan::call('migrate:fresh --seed');
+    return "Successfully Reseted";
+});
+
 require __DIR__.'/auth.php';
