62f7a3f2ad2e7e33b5b1c65081c1a6793c4a4933
Jeffrey Gepiga <jeffreygepiga27@gmail.com>
2025-04-02 00:39:40 +0800

    sd

--- app/Http/Controllers/ServiceController.php
+++ app/Http/Controllers/ServiceController.php
@@ -17,7 +17,7 @@ class ServiceController extends Controller
     // Show the form to add a new service
     public function create()
     {
-        return view('admin.add-service');
+        return view('admin.addService');
     }
 
     // Store a new service
