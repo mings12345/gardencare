8db95a657f52872893ea07b7aa64b8776fa3b8b7
Jeffrey Gepiga <jeffreygepiga27@gmail.com>
2025-04-19 15:02:25 +0800

    Updated

--- config/sanctum.php
+++ config/sanctum.php
@@ -17,7 +17,7 @@ return [
 
     'stateful' => explode(',', env('SANCTUM_STATEFUL_DOMAINS', sprintf(
         '%s%s%s',
-        'localhost,localhost:3000,127.0.0.1,127.0.0.1:3000,127.0.0.1:8000,::1',
+        'localhost,localhost:3000,127.0.0.1,127.0.0.1:3000,127.0.0.1:8000,::1,devjeffrey.dreamhosters.com',
         Sanctum::currentApplicationUrlWithPort(),
         '*.dreamhosters.com',
         'devjeffrey.dreamhosters.com',
