0f95c834bd5618bcb97162862dd87df8798ec93c
Jeffrey Gepiga <jeffreygepiga27@gmail.com>
2025-04-19 15:00:44 +0800

    Updated

--- app/Http/Middleware/VerifyCsrfToken.php
+++ app/Http/Middleware/VerifyCsrfToken.php
@@ -14,6 +14,7 @@ class VerifyCsrfToken extends Middleware
     protected $except = [
         'api/*',              // All API routes
         'broadcasting/auth',  // Pusher auth endpoint
-        'sanctum/csrf-cookie' // Sanctum's CSRF cookie route
+        'sanctum/csrf-cookie', // Sanctum's CSRF cookie route
+        'webhook/*'
     ];
 }
