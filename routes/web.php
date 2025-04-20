7ca1ed7354275f918f11648573742c8a17ff2926
Jeffrey Gepiga <51604875+JeffGepiga@users.noreply.github.com>
2025-04-21 00:50:10 +0800

    Updated

--- routes/web.php
+++ routes/web.php
@@ -96,13 +96,12 @@ Route::get('/update-app', function () {
 });
 
 Route::get('/reset-app', function () {
-        $process = new \Symfony\Component\Process\Process(['php', 'artisan', 'migrate:fresh', '--seed']);
-        $process->setInput("Y\n"); // Automatically sends "Y" and Enter
-        $process->run();
-        if (!$process->isSuccessful()) {
-            throw new \RuntimeException($process->getErrorOutput());
-        }
-        echo $process->getOutput();
+    \Artisan::call('migrate:fresh', [
+        '--seed'  => true,
+        '--force' => true, 
+    ]);
+    
+    echo \Artisan::output();
 });
 
 require __DIR__.'/auth.php';
