946f2f6ee667f4789db5eaa7f376cc1f16f74f57
Jeffrey Gepiga <jeffreygepiga27@gmail.com>
2025-04-26 15:46:29 +0800

    Updated

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -142,6 +142,7 @@ class AuthController extends Controller
             'email' => 'sometimes|string|email|max:255|unique:users,email,' . $user->id,
             'phone' => 'sometimes|string|max:15',
             'address' => 'sometimes|string|max:255',
+            'gcash' => 'sometimes|string|max:11',
         ]);
 
         if ($validator->fails()) {
@@ -152,7 +153,7 @@ class AuthController extends Controller
         }
 
         // Update user profile
-        $user->update($request->only(['name', 'email', 'phone', 'address']));
+        $user->update($request->only(['name', 'email', 'phone', 'address,', 'gcash']));
 
         return response()->json([
             'message' => 'Profile updated successfully.',
--- database/migrations/2025_03_10_170910_create_payments_table.php
+++ database/migrations/2025_03_10_170910_create_payments_table.php
@@ -14,8 +14,7 @@ return new class extends Migration
         Schema::create('payments', function (Blueprint $table) {
             $table->id();
             $table->foreignId('booking_id')->constrained()->onDelete('cascade');
-            $table->string('payment_type');
-            $table->string('payment_method');
+            $table->enum('payment_method',['Gcash', 'Card']);
             $table->decimal('amount_paid', 10, 2);
             $table->decimal('remaining_balance', 10, 2)->default(0);
             $table->timestamp('payment_date');
