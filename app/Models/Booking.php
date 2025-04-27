efb51bb2d26e3eaea08377be37b77aeab9d1b464
Jeffrey Gepiga <jeffreygepiga27@gmail.com>
2025-04-27 15:56:30 +0800

    sdads

--- app/Models/Booking.php
+++ app/Models/Booking.php
@@ -14,19 +14,21 @@ class Booking extends Model
     // Define the relationship with the Homeowner (User Model)
     public function homeowner()
     {
-        return $this->belongsTo(User::class, 'homeowner_id', 'id');
+        return $this->hasOne(User::class, 'id', 'homeowner_id');
     }
 
     // Define the relationship with the Gardener (assuming it's also a User)
     public function gardener()
     {
-        return $this->belongsTo(User::class, 'gardener_id', 'id');
+        return $this->hasOne(User::class, 'id', 'gardener_id')
+        ->where('type', 'Gardening');
     }
 
     // Define the relationship with the Service Provider (assuming it's also a User)
     public function serviceProvider()
     {
-        return $this->belongsTo(User::class, 'serviceprovider_id', 'id');
+        return $this->hasOne(User::class,  'id', 'serviceprovider_id')
+        ->where('type', 'Landscaping');
     }
     public function payments()
     {
