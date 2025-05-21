ed563b6b1b4247d066135d31d1bdcf85efcd072b
mings12345 <minguitojohnlloyd@gmail.com>
2025-05-21 10:33:38 +0800

    gsdsd

--- app/Http/Controllers/ServiceProviderController.php
+++ app/Http/Controllers/ServiceProviderController.php
@@ -5,6 +5,8 @@ namespace App\Http\Controllers;
 use App\Http\Controllers\Controller;
 use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
+
 
 class ServiceProviderController extends Controller
 {
