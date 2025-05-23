774b63ea8fc353afe92e2e61f54b4e1073b2f08f
mings12345 <minguitojohnlloyd@gmail.com>
2025-05-23 08:38:46 +0800

    fdfs

--- database/migrations/2025_01_30_133623_create_services_table.php
+++ database/migrations/2025_01_30_133623_create_services_table.php
@@ -13,7 +13,7 @@ return new class extends Migration
     {
         Schema::create('services', function (Blueprint $table) {
             $table->id();
-            $table->foreignId('user_id')->constrained()->onDelete('cascade');
+            $table->foreignId('user_id')->nullable()->constrained()->onDelete('cascade');
             $table->enum('type',['Landscaping','Gardening']);
             $table->string('name'); // e.g., Lawn Mowing, Tree Trimming
             $table->text('description')->nullable();
